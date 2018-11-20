7532543fd5ea55db4baf8dce5657e9f5f220df87
Neelam Verma <neelamverma@Neelams-MacBook-Pro.local>
2018-11-20 20:14:38 +0530

    Matrix Rows Reverse Code

---
+++ reverseMatrixRows/reverseMatrixRows/Matrix.swift
@@ -0,0 +1,75 @@
+//
+//  Matrix.swift
+//  reverseMatrixRows
+//
+//  Created by Neelam Verma on 20/11/18.
+//  Copyright © 2018 Neelam Verma. All rights reserved.
+//
+
+import UIKit
+
+class Matrix {
+    let rows , columns: Int
+    var grid: [Int]
+    
+    fileprivate init(rows: Int, columns: Int) {
+        self.rows = rows
+        self.columns = columns
+        self.grid = Array(repeating: 0, count: rows * columns)
+    }
+    
+    subscript(rowIndex: Int , columnIndex: Int) -> Int {
+        get {
+            return grid[rowIndex * columns + columnIndex]
+        }
+        set (newValue) {
+            grid[rowIndex * columns + columnIndex] = newValue
+        }
+    }
+    
+    func giveMeStringToDisplay() -> String {
+        var outputString = ""
+        for i in 0..<self.rows {
+            for j in 0..<self.columns {
+                outputString += String(self[i,j])
+                outputString += ","
+            }
+            outputString += "\n"
+        }
+        return outputString
+    }
+    
+    init?(input : String){
+        
+        if input == "" { return nil }
+        
+        let rows = input.components(separatedBy: .newlines)
+        let numberOfRows = rows.count
+        let digitsArray = rows[0].components(separatedBy: ",")
+        let numberOfColumns = digitsArray.count
+        
+        let matrix = Matrix(rows: numberOfRows, columns: numberOfColumns)
+        
+        for i in 0..<rows.count {
+            let digitsArray = rows[i].components(separatedBy: ",")
+            for j in 0..<digitsArray.count {
+                let value = digitsArray[j].trimmingCharacters(in: .whitespacesAndNewlines)
+                
+                if let num = Int(value) {
+                    if j > numberOfColumns - 1 {
+                        return nil
+                    }
+                    
+                    matrix[i,j] = num
+                }
+                else {
+                    return nil
+                }
+            }
+        }
+        self.rows = matrix.rows
+        self.columns = matrix.columns
+        self.grid = matrix.grid
+        
+    }
+}
---
+++ reverseMatrixRows/reverseMatrixRows/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  reverseMatrixRows
+//
+//  Created by Neelam Verma on 20/11/18.
+//  Copyright © 2018 Neelam Verma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var inputMatrixTextView: UITextView!
+    @IBOutlet weak var reverseMatrixtextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func reverseClickAction(_ sender: Any) {
+        guard let input = createTheInputMatrix(arrString: inputMatrixTextView.text) else {
+            let alert = UIAlertController.init(title: "Error", message: "Invalid Matrix", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            return
+        }
+        reverseMatrixtextView.text = reverseMatrixRows(mat: input).giveMeStringToDisplay()
+    }
+    
+    @IBAction func resetForm(_ sender: Any) {
+        inputMatrixTextView.text = ""
+        reverseMatrixtextView.text = ""
+    }
+    
+    func createTheInputMatrix(arrString: String) -> Matrix? {
+        return Matrix(input: arrString)
+    }
+    
+    func reverseMatrixRows(mat: Matrix) -> Matrix {
+        for row in 0..<mat.rows {
+            for col in 0...Int(mat.columns/2) {
+                let a = mat[row, col]
+                mat[row, col] = mat[row, (mat.columns - col - 1)]
+                mat[row, (mat.columns - col - 1)] = a
+            }
+        }
+        return mat
+    }
+}
+
